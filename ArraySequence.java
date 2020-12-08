3c9aaa08a941a1b68afd80002d0a93e3c3edf73f
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-12-08 11:37:47 -0500

    new file

---
+++ ArraySequence.java
@@ -0,0 +1,11 @@
+import java.util.NoSuchElementException;
+
+Public class ArraySequence implements IntegerSequence{
+  private int currentIndex;
+  private int []data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+     
+  }
+}
